85adf5f53ddd4e613fe1b380711007d36cb06fa1
Unknown <liamoreno14@gmail.com>
2018-08-18 18:47:40 -0300

    Creacion clases
    creado de clases con comentarios de metodos a programar

---
+++ src/ControlGestion.java
@@ -0,0 +1,11 @@
+
+public interface ControlGestion {
+	
+	////metodo a codificar: 
+	//					verificarDisponiblePista(
+	//					asignarPista()
+	//					desasignarPista()
+	//					asignarTurno()
+	
+
+}
