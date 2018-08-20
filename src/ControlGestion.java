162add2c684f6e011f9cd14a60f02c7ea4390e04
giuz <guly_09_@hotmail.com>
2018-08-20 16:25:31 -0300

    agregue cosas para interfaz son pocas cositas

--- src/ControlGestion.java
+++ src/ControlGestion.java
@@ -1,5 +1,9 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Stack;
 
 public interface ControlGestion {
+	
 	
 	////metodo a codificar: 
 	//					verificarDisponiblePista(
@@ -7,5 +11,12 @@ public interface ControlGestion {
 	//					desasignarPista()
 	//					asignarTurno()
 	
+	boolean verificarDisponiblePista(boolean array[]) throws RemoteException;
+	Integer asignarPista(boolean array[]) throws RemoteException;
+	Integer desasignarPista(boolean array[]) throws RemoteException;
+	Stack<Boolean> asignarTurno() throws RemoteException;
+	
 
+	
+	
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,4 +1,30 @@
+import java.rmi.RemoteException;
+import java.util.Stack;
 
 public class Servidor implements ControlGestion {
+
+	@Override
+	public boolean verificarDisponiblePista(boolean[] array) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Integer asignarPista(boolean[] array) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer desasignarPista(boolean[] array) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Stack<Boolean> asignarTurno() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
