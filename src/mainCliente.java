ab9f8e1650ee87802c7ca7293fe7b6d80c33219e
Unknown <liamoreno14@gmail.com>
2018-09-02 00:47:17 -0300

    Codificacion lanzador de aviones

--- src/mainCliente.java
+++ src/mainCliente.java
@@ -1,10 +1,27 @@
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class mainCliente {
 	public static void main(String[] args) throws RemoteException, NotBoundException {
 		Cliente c = new Cliente("192.168.237.229", 2000);
 	
+		/////////////////////////////////////
+		Random tiempoRandom = new Random ();
+		Timer time = new Timer();
+		TimerTask lanzadorAvion = new TimerTask() {
+	
+			public void run() {
+				System.out.println("Se lanzo un avion");
+					
+			}
+		};	
+		time.schedule(lanzadorAvion, tiempoRandom.nextInt(10)); //cuenta un tiempo random y llama al run de contador	
+		////////////////////////////////////////////
+	
 	}
-
+	
+	
 }
